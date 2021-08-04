72550de112a1a219c21ec83d68c36717d989540c
José Luís Martins <zeluis0712@gmail.com>
2021-08-04 22:37:55 +0100

    login feature added

--- app/resources/auth.py
+++ app/resources/auth.py
@@ -1,16 +1,38 @@
 import logging
-
-from flask import jsonify
-from flask_restful import Resource, reqparse
+from base64 import b64decode
+from datetime import timedelta
 
 from app.extension import db
 from app.models import User
+from flask import jsonify, request
+from flask_jwt_extended import create_access_token
+from flask_restful import Resource, reqparse
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 class Login(Resource):
 
     def get(self):
-        return {"pagina": "Login"}
+        if not request.headers.get("Authorization"):
+            return {"error": "authorization not found"}, 400
+
+        basic, code = request.headers["Authorization"].split(" ")
+        print(code)
+        if not basic.lower() == "basic":
+            return {"error": "bad authorization"}, 400
+
+        email, password = b64decode(code).decode().split(":")
+        print("email", email)
+        print("password", password)
+        user = User.query.filter_by(email=email).first()
+        if not user or not user.password == password:
+            return {
+                "error": "authentication fails! user or password are incorrect"
+            }, 400
+
+        token = create_access_token({"id": user.id}, expires_delta=timedelta(minutes=5))
+
+        return {"encoded_token": token}
 
 
 class Signup(Resource):
