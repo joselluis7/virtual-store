643bbeaedc313d1ecc1b8d41b9299d3519c78259
José Luís Martins <zeluis0712@gmail.com>
2021-08-11 22:43:09 +0100

    Order and Item Models Added

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
-from enum import unique
 from datetime import datetime
 
 from sqlalchemy.orm import backref
+
 from app.extension import db
+from app.enums import EStatus
 
 
 class User(db.Model):
@@ -45,4 +46,28 @@ class Category(db.Model):
     def __repr__(self):
         return self.name
 
-    
+class Order(db.Model):
+
+    __tablename__ = "orders"
+
+    id = db.Column(db.Integer, primary_key=True)
+    order_number = db.Column(db.String(20), unique=True, nullable=False)
+    status = db.Column(db.String(30), default=EStatus.CREATED.value, nullable=False )
+    item = db.relationship("Item", backref="order", uselist=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return f"order number: {self.order_number}"
+
+class Item(db.Model):
+    __tablename__ = "order_items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    prduct_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey("orders.id"), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return (f"order number: ${self.order_number} quantity: {self.quantity} user: {self.user.profile.first_name} {self.user.profile.last_name}" )
