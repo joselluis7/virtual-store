0a6e63be5238bb34fbe608b90d35b09b8ed10363
José Luís Martins <zeluis0712@gmail.com>
2021-08-11 22:46:04 +0100

    enum order status created

---
+++ app/enums/EStatus.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class EStatus(Enum):
+    CREATED = "created"
+    EXPIRED = "payment time exceed"
+    ANALYSIS = "analysing"
+    COMPLETE = "completed"
+    CARGEBACK = "chargeback"
+    PAID = "paid"
+    REFUNDED = "refunded"
+    FAILED = "failed"
