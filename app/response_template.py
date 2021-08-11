50623b26dc27b4442dc96a39be6ce65ca404d3ef
José Luís Martins <zeluis0712@gmail.com>
2021-08-11 21:22:52 +0100

    categories serialized

--- app/response_template.py
+++ app/response_template.py
@@ -1,11 +1,19 @@
 from flask_restful import fields
 
 
+categories_fields = {
+
+    "name": fields.String,
+    "slug": fields.String
+
+}
+
 products_fields = {
     "id" : fields.Integer,
     "name": fields.String,
     "slug": fields.String, 
     "price": fields.Float,
     "quantity":fields.Integer,
-    "description": fields.String
+    "description": fields.String,
+    "categories": fields.Nested(categories_fields)
 }
